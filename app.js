b6d2900d49f0b2090b27695cba60ef1cbad84d43
Laserjonge <axelderuiter@gmail.com>
2022-03-03 21:45:16 +0100

    worked on template helpers

--- app.js
+++ app.js
@@ -1,51 +1,64 @@
 console.log('hi');
 
-// import express from 'express';
-// import { engine } from 'express-handlebars';
 
-// const app = express();
+const express = require('express');
+const {engine} = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-// app.engine('handlebars', engine());
-// app.set('view engine', 'handlebars');
-// app.set('views', './views');
-
-// app.get('/home', (req, res) => {
-//     res.render('home');
-// });
-
-// app.listen(3000);
-
-
-
-const express = require('express')
-const {engine} = require('express-handlebars')
-const app = express()
-const port = 3000
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-// import { engine } from 'express-handlebars';
-
 app.use(express.static(__dirname + "/static"));
 
 app.get('/', (req, res) => {
-  res.render('Home')
-})
+  res.render('Home', {
+    title: 'Home Page',
+    name: 'Axel de Ruiter',
+    age: 21,
+    isDisplayName: true,
+    isAgeEnabled: true
+  });
+});
 
 app.get('/about', (req, res) => {
-  res.render('About')
-})
+  res.render('About', {
+    person: {
+      firstname: "Axel",
+      lastname: "de Ruiters",
+    },
+    title: "About"
+  });
+});
 
 app.get('/settings', (req, res) => {
-  res.send('Verander hier jouw instellingen')
-})
+  res.render('Settings');
+});
+
+app.get('/dashboard', (req, res) => {
+  res.render('Dashboard', {
+    isListEnabled: true
+  });
+});
+
+app.get('/each/helper', (req, res) => {
+  res.render('Contact', {
+    people: [
+      "James",
+      "Peter",
+      "Sadrack",
+      "Morissa"
+    ]
+  });
+});
 
 app.get('*', (req, res) => {
-  res.send('404, niets gevonden')
-})
+  res.send('404, niets gevonden');
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
