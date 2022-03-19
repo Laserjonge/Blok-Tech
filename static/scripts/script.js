0c48408b7938c6dacd3c40fade51a13d8967814d
Laserjonge <axelderuiter@gmail.com>
2022-03-19 01:32:58 +0100

    script.js ES5 to ES6

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,16 +1,27 @@
-console.log("Hi...");
+/////////
+// ES5 //
+/////////
+// var settingsButton = document.querySelector(".settings-button")
 
-var settingsButton = document.querySelector(".settings-button")
+// settingsButton.addEventListener("click", toggleMenu);
 
-settingsButton.addEventListener("click", toggleMenu);
+// function toggleMenu() {
+//   var form = document.querySelector(".pop-up-form");
+//   form.classList.toggle("pop-up-function");
+// }
 
-function toggleMenu() {
-  var form = document.querySelector(".pop-up-form");
-  form.classList.toggle("pop-up-function");
-}
+// function settingsButtonPosition() {
+//   settingsButton.classList.add("button-position");
+// }
 
-function settingsButtonPosition() {
-  settingsButton.classList.add("button-position");
-}
+// settingsButtonPosition();
 
-settingsButtonPosition();
+
+/////////
+// ES6 //
+/////////
+const settingsButton = document.querySelector(".settings-button")
+
+settingsButton.addEventListener("click", toggleMenu = () => document.querySelector(".pop-up-form").classList.toggle("pop-up-function"));
+
+settingsButtonPosition = (settingsButton.classList.add("button-position"));
